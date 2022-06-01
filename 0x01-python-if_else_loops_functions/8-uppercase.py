c34ab24f168e68757771867204e42b55e87f62cd
Blackmile <stanley.ezeifeanyi@gmail.com>
2022-06-01 08:05:06 -0700

    fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,8 +2,8 @@
 def uppercase(str):
     for i in str:
         if ord(i) >= 97 and ord(i) <= 122:
-            l = ord(i) - 32
+            ln = ord(i) - 32
         else:
-            l = ord(i)
+            ln = ord(i)
         print("{:c}".format(l), end="")
     print("")
