a820f5b35a3ec186825302a10d0ad72f204b602f
Blackmile <stanley.ezeifeanyi@gmail.com>
2022-06-06 10:05:24 -0700

    removed if condition

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
-        newList = my_string[:]
-        for i in newList:
-            if i == 'c' or i == 'C':
-                newList.pop(i)
-            else:
-                return newList
+    newList = my_string[:]
+    for i in newList:
+        if i == 'c' or i == 'C':
+            newList.pop(i)
+        else:
+            return newList
