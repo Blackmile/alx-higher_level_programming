56c8e9198ea75bf028f3aefadf7cd3ec3fe97b69
Blackmile <stanley.ezeifeanyi@gmail.com>
2022-06-06 09:52:37 -0700

    fix 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    else:
-        my_list.insert(idx, element)
-        return my_list
+    if my_list:
+        newList = my_list[:]
+        if idx < 0 or idx >= len(my_list):
+            return newList
+        else:
+            newList[idx] = element
+            return newList
