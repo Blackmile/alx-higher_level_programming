1f6152b99ca05a5109467bb20ae0f0f9145e7b0b
Blackmile <stanley.ezeifeanyi@gmail.com>
2022-06-21 20:54:32 -0700

    private size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """ This sqaure class defines a square size private """
+    def __init__(self, size):
+        """ initializes the object """
+        self.__size = size
